2e28fe368e02dcdd4b504db0251fcff7cc1e54f6
shifa-anwar <98535123+shifa-anwar@users.noreply.github.com>
2022-07-09 12:31:00 +0530

    Update ValidAnagram.java

--- String/ValidAnagram.java
+++ String/ValidAnagram.java
@@ -1,3 +1,27 @@
+1.using one array for hashing
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    if(s.length()!=t.length())
+    {
+      return false;  
+    }
+    int hash[]=new int[26];
+    for(int i=0;i<s.length();i++)
+    {
+     hash[s.charAt(i)-'a']++;
+     hash[t.charAt(i)-'a']--;
+    }
+    for(int i=0;i<26;i++)
+    {
+     if(hash[i]!=0)
+     {
+       return false;
+     }
+    }
+        return true;
+    }
+}
+2.
 class Solution {
     public boolean isAnagram(String s, String t) {
         int hash1[]=new int[128];
